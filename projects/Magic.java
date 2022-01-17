28b7aac2c1a38e0137010702b961b6c58a184cb6
Andreas Skotadis <skottydeveloper@gmail.com>
2022-01-17 21:46:16 +1100

    Slight tweak to Magic project.

--- projects/Magic.java
+++ projects/Magic.java
@@ -2,7 +2,7 @@ public class Magic {
     public static void main(String[] args) {
         // myNumber is the original number.
         final int myNumber = 2;
-            int stepOne = myNumber * myNumber;
+        int stepOne = myNumber * myNumber;
         int stepTwo = stepOne + myNumber;
         int stepThree = stepTwo / myNumber;
         int stepFour = stepThree + 17;
