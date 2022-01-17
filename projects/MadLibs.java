b4d5e753ee7823f204df8df418d082e5f1a0acec
Andreas Skotadis <skottydeveloper@gmail.com>
2022-01-17 21:39:32 +1100

    Slight tweaks to MadLibs project.

--- projects/MadLibs.java
+++ projects/MadLibs.java
@@ -23,7 +23,7 @@ public class MadLibs {
         place1 = "Australia";
         number = 2250;
       
-        String story = "This morning " + name1 + " woke up feeling "+ adjective1 + ". 'It is going to be a " + adjective2 + " day!' Outside, a bunch of " + noun1 + " were protesting to keep " + noun2 + " in stores. They began to " + verb1 + " to the rhythm of the " + noun3 + ", which made all the " + noun4 + " very " + adjective3 + ". Concerned, " + name1 + " texted " + name2 + ", who flew " + name1 + " to " + place1 + " and dropped him" + " in a puddle of frozen " + noun5 + ". " + name1 + " woke up in the year " + number +", in a world where " + noun6 + " ruled the world.";
+        String story = "This morning " + name1 + " woke up feeling " + adjective1 + ". 'It is going to be a " + adjective2 + " day!' Outside, a bunch of " + noun1 + " were protesting to keep " + noun2 + " in stores. They began to " + verb1 + " to the rhythm of the " + noun3 + ", which made all the " + noun4 + " very " + adjective3 + ". Concerned, " + name1 + " texted " + name2 + ", who flew " + name1 + " to " + place1 + " and dropped him" + " in a puddle of frozen " + noun5 + ". " + name1 + " woke up in the year " + number + ", in a world where " + noun6 + " ruled the world.";
       
         System.out.println(story);
     }         
