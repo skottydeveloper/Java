a2b36108875c66f58bbc8ea737a93d6169d6f810
Andreas Skotadis <skottydeveloper@gmail.com>
2022-01-17 18:43:00 +1100

    Import tree mini-project.

---
+++ projects/Tree.java
@@ -0,0 +1,14 @@
+public class Tree {
+    public static void main(String[] args) {
+      // We're going to introduce ourselves, and print a tree for the user.
+      System.out.println("Hi there, I'm Andreas. I'm revising what I've learned in Java, so far!");
+      System.out.println("I'm in the mood to plant a tree today!"); 
+  
+      // Tree planting time!
+      System.out.println("  *  ");
+      System.out.println(" *** ");
+      System.out.println(" *** ");
+      System.out.println("  *  ");
+      System.out.println("  *  ");   
+    }
+  }
