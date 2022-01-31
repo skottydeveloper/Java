589057bfec2744b718607e7931118728a1ec255d
Andreas Skotadis <skottydeveloper@gmail.com>
2022-01-31 18:52:19 +1100

    Upload Language Families project.

---
+++ java_mini_projects/LanguageFamilies/Language.java
@@ -0,0 +1,34 @@
+package java_mini_projects.LanguageFamilies;
+
+class Language {
+    protected String name;
+    protected int numSpeakers;
+    protected String regionsSpoken;
+    protected String wordOrder;
+  
+    public Language(String langName, int speakers, String regions, String order) {
+        this.name = langName;
+        this.numSpeakers = speakers;
+        this.regionsSpoken = regions;
+        this.wordOrder = order;
+    }
+  
+    public void getInfo() {
+        System.out.println(this.name + " is spoken by " + this.numSpeakers + " people mainly in " + this.regionsSpoken + ".");
+        System.out.println("The language follows the word order: " + this.wordOrder);
+    }
+  
+    public static void main(String[] args) {
+        Language spanish = new Language("Spanish", 555000000, "Spain, Latin America, and Equatorial Guinea", "subject-verb-object");
+        spanish.getInfo();
+  
+        Mayan kiche = new Mayan("Ki'che'", 2330000);
+        kiche.getInfo();
+  
+        SinoTibetan mandarin = new SinoTibetan("Mandarin Chinese", 1110000000);
+        mandarin.getInfo();
+    
+        SinoTibetan burmese = new SinoTibetan("Burmese", 43000000);
+        burmese.getInfo();
+    }
+}
